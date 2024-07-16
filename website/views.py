a6e19f53b2a6cffecfc2b9955b3b1aaefcde7328
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-07-16 16:52:24 +0200

    I am confused git

--- website/views.py
+++ website/views.py
@@ -9,16 +9,6 @@ views = Blueprint("views", __name__)
 @views.route("/", methods=["GET", "POST"])
 @login_required
 def home():
-    if request.method == "POST":
-        note = request.form.get("note")
-
-        if len(note) < 1:
-            flash("Note is empty", category="error")
-        else:
-            new_note = Note(data=note, user_id=current_user.id)
-            db.session.add(new_note)
-            db.session.commit()
-            flash("Note added", category="success")
     return render_template("home.html", user=current_user)
 
 @views.route("/account", methods=["GET", "POST"])
