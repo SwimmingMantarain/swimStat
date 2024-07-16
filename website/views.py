c6ef1a1ce9fccae61fad08c896625ac931ae45ec
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-07-16 16:37:32 +0200

    some progress towards account deletion

--- website/views.py
+++ website/views.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, request, flash, jsonify
+from flask import Blueprint, render_template, request, flash, jsonify, redirect, url_for
 from flask_login import login_required, current_user
-from .models import Note
+from .models import User
 from . import db
 import json
 
@@ -37,3 +37,17 @@ def delete_note():
             db.session.commit()
             flash("Note deleted", category="success")
     return jsonify({})
+
+@views.route("/delete-account", methods=["POST", "GET"])
+@login_required
+def delete_account():
+    user = json.loads(request.data)
+    userID = user["userId"]
+    user = User.query.get(userID)
+    if user:
+        if user.id == current_user.id:
+            db.session.delete(user)
+            db.session.commit()
+            flash("Account deleted", category="success")
+
+    return redirect(url_for('auth.login'))
