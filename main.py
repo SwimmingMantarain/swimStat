2cd6856166eb43db9877441210c440da11967409
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-07-18 12:59:27 +0200

    added add_session.html and maybe stopped server from carshing every time I edit file and it thinks there is a syntax error

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import sys
 from website import create_app
 
 app = create_app()
 
+if "--ignore-errors" in sys.argv:
+    sys.argv.remove("--ignore-errors")
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
--- website/__init__.py
+++ website/__init__.py
@@ -3,37 +3,43 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-db = SQLAlchemy()
-DB_NAME = "database.db"
+db = SQLAlchemy()  # Create SQLAlchemy instance
+DB_NAME = "database.db"  # Set database name
+
 
 def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "qaplweiucalkjdsfhaloei"
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"
-    db.init_app(app)
+    """Create and configure Flask app."""
+    app = Flask(__name__)  # Create Flask app instance
+    app.config["SECRET_KEY"] = "qaplweiucalkjdsfhaloei"  # Set secret key for app
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{DB_NAME}"  # Set database uri
+    db.init_app(app)  # Initialize SQLAlchemy with app
 
-    login_manager = LoginManager()
-    login_manager.login_view = "auth.login"
-    login_manager.init_app(app)
+    login_manager = LoginManager()  # Create LoginManager instance
+    login_manager.login_view = "auth.login"  # Set login view
+    login_manager.init_app(app)  # Initialize LoginManager with app
 
+    # Register blueprints
     from .views import views
     from .auth import auth
     from .training import training
 
-    app.register_blueprint(views, url_preifx="/")
+    app.register_blueprint(views, url_prefix="/")
     app.register_blueprint(auth, url_prefix="/")
     app.register_blueprint(training, url_prefix="/")
 
     from .models import User, Block, BlockOfBlocks, TrainingSession
-    create_database(app)
+    create_database(app)  # Create database if it doesn't exist
 
     @login_manager.user_loader
     def load_user(id):
+        """Load user by ID."""
         return User.query.get(int(id))
 
     return app
 
+
 def create_database(app):
+    """Create database if it doesn't exist."""
     if not path.exists("website/" + DB_NAME):
         with app.app_context():
             db.create_all()
