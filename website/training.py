b8c387eb778f74d5a916747fb68bf7d4ef64116c
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-07-29 14:19:57 +0200

    missing variable

--- website/training.py
+++ website/training.py
@@ -9,14 +9,14 @@ training = Blueprint("training", __name__)
 @training.route("/add-session")
 @login_required
 def add_session():
-    return render_template("add_session.html")
+    return render_template("add_session.html", user=current_user)
 
 @training.route("/edit-session")
 @login_required
 def edit_session():
-    return render_template("edit_session.html")
+    return render_template("edit_session.html", user=current_user)
 
 @training.route("/delete-session")
 @login_required
 def delete_session():
-    return render_template("delete_session.html")
+    return render_template("delete_session.html", user=current_user)
