52e5f08b471d797e54dde58d6ddcadd7a305d0db
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-08-07 13:16:59 +0200

    I just pleasing myself

--- sync.sh
+++ sync.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
-# This script will sync this project to my server which is running on
-# 192.168.1.32 and the directory for it is /var/www/webApp/webApp
+
+clear
+echo -e "\n\033[1m\033[4m\033[33mSyncing project to server...\033[0m"
 
 # Sync and replace existing files with rsync and ssh as root
 
 # main.py
-rsync -avzP --progress main.py root@192.168.1.32:/var/www/webApp/webApp
+echo -e "\033[33mSyncing main.py...\033[0m"
+rsync -azP --no-o --no-g --no-p main.py root@192.168.1.32:/var/www/webApp/webApp > /dev/null
 
 # website folder
-rsync -avzP --progress website root@192.168.1.32:/var/www/webApp/webApp
+echo -e "\033[33mSyncing website folder...\033[0m"
+rsync -azP --no-o --no-g --no-p website root@192.168.1.32:/var/www/webApp/webApp > /dev/null
 
 # restart apache2 server
-ssh root@192.168.1.32 'systemctl restart apache2'
+echo -e "\033[1m\033[32mRestarting apache2 server...\033[0m"
+ssh root@192.168.1.32 'systemctl restart apache2' > /dev/null
+echo -e "\033[1m\033[32mDone!\033[0m"
+
