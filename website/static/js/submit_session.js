eefbd89b2412a32ee96ab80969b1ac89c638b048
SwimmingMantarain <swimmingMantarain@gmail.com>
2024-08-24 09:38:28 +0200

    can't figure out how to insert a vlue from html into jinja2, hmmmmmm ;)

--- website/static/js/submit_session.js
+++ website/static/js/submit_session.js
@@ -10,6 +10,21 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
 btn.onclick = function () {
     modal.style.display = "block";
+    var today = new Date();
+    var dd = String(today.getDate()).padStart(2, "0");
+    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+    var yyyy = today.getFullYear();
+    var hours = String(today.getHours()).padStart(2, "0");
+    var minutes = String(today.getMinutes()).padStart(2, "0");
+
+    today = yyyy + "-" + mm + "-" + dd;
+    var time = hours + ":" + minutes;
+    document.getElementById("dateInput").value = today;
+    document.getElementById("timeInput").value = time;
+
+    block = btn.parentElement;
+    session = block.children[5];
+    document.getElementById("sessionInput").value = session.id;
 }
 
 // When the user clicks on <span> (x), close the modal
