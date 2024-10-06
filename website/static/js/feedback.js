6a157a7d58455f5736f33ce28411471b1d28941b
SwimmingMantarain <swimmingmantarain@gmail.com>
2024-10-06 10:47:05 +0200

    set up for creating a rust version with loco crate

--- website/static/js/feedback.js
+++
@@ -1,9 +0,0 @@
-async function sendFeedback(data) {
-    console.log(data)
-    const response = await fetch("/feedback", {
-        method: "POST",
-        body: JSON.stringify({ data: data })
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
